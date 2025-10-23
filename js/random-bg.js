6b07de30b50531efc3139ffbf8dc91e723b6d947
ldn970110 <ldn970110@gmail.com>
2025-10-23 14:49:05 +0800

    Site updated: 2025-10-23 14:49:05

--- js/random-bg.js
+++ js/random-bg.js
@@ -1,41 +1,21 @@
 (function() {
   // 讀取明亮與暗色模式背景圖資料夾路徑
   const lightImages = [
-  '/img/bg-light/bg-light-1.webp',
-  '/img/bg-light/bg-light-2.webp',
-  '/img/bg-light/bg-light-3.webp',
-  '/img/bg-light/bg-light-4.webp',
-  '/img/bg-light/bg-light-5.webp',
-  '/img/bg-light/bg-light-6.webp',
-  '/img/bg-light/bg-light-7.webp',
-  '/img/bg-light/bg-light-8.webp',
-  '/img/bg-light/bg-light-9.webp',
-  '/img/bg-light/bg-light-10.webp',
-  '/img/bg-light/bg-light-11.webp',
-  '/img/bg-light/bg-light-12.webp',
-  '/img/bg-light/bg-light-13.webp',
-  '/img/bg-light/bg-light-14.webp',
-  '/img/bg-light/bg-light-15.webp'
+  '/img/bg-dark/bg-1.webp',
+  '/img/bg-dark/bg-2.webp',
+  '/img/bg-dark/bg-3.webp',
+  '/img/bg-dark/bg-4.webp',
+  '/img/bg-dark/bg-5.webp',
+  '/img/bg-dark/bg-6.webp'
 ];
 
   const darkImages = [
-  '/img/bg-dark/bg-dark-1.webp',
-  '/img/bg-dark/bg-dark-2.webp',
-  '/img/bg-dark/bg-dark-3.webp',
-  '/img/bg-dark/bg-dark-4.webp',
-  '/img/bg-dark/bg-dark-5.webp',
-  '/img/bg-dark/bg-dark-6.webp',
-  '/img/bg-dark/bg-dark-7.webp',
-  '/img/bg-dark/bg-dark-8.webp',
-  '/img/bg-dark/bg-dark-9.webp',
-  '/img/bg-dark/bg-dark-10.webp',
-  '/img/bg-dark/bg-dark-11.webp',
-  '/img/bg-dark/bg-dark-12.webp',
-  '/img/bg-dark/bg-dark-13.webp',
-  '/img/bg-dark/bg-dark-14.webp',
-  '/img/bg-dark/bg-dark-15.webp'
+  '/img/bg-dark/bg-1.webp',
+  '/img/bg-dark/bg-2.webp',
+  '/img/bg-dark/bg-3.webp',
+  '/img/bg-dark/bg-4.webp',
+  '/img/bg-dark/bg-5.webp'
 ];
-
   // 取得隨機圖片
   function getRandomImage(images) {
     return images[Math.floor(Math.random() * images.length)];
